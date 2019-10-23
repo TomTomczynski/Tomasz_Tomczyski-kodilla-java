8c1b93823a4b03dc1c9b4b125af570eaa011c50c
TomTomczynski <tom.tomczynski@gmail.com>
2019-10-23 20:26:20 +0200

    Herokuv2

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -9,7 +9,6 @@ public class TasksApplication {//extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(TasksApplication.class, args);
     }
-
 //    @Override
 //    protected SpringApplicationBuilder configure (SpringApplicationBuilder application)
 //    {
